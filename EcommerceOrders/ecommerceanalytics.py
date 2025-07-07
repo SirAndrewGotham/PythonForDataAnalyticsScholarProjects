c1e9ae6e8895b927504750ae0f78786f03515c1f
AndrewGotham <andreogotema@gmail.com>
2025-07-07 08:57:19 +0300

    Read data from files and analyze to see if any null data exists

--- EcommerceOrders/ecommerceanalytics.py
+++ EcommerceOrders/ecommerceanalytics.py
@@ -3,6 +3,22 @@ Ecommerce orders analyses project, see common README.md for a complete descripti
 """
 
 import os
+import pandas as pd
 
-source_files_path = os.getcwd() + '\\SourceFiles'
-print(source_files_path)
+# prepare a variable for the relative path to the source excel files
+source_files_path = os.getcwd() + '\\EcommerceOrders\\SourceFiles'
+
+# ======================================================================================================================
+# Loading files
+# ======================================================================================================================
+customers_data = pd.read_excel(source_files_path + '\\customers.xlsx')
+orders_data = pd.read_excel(source_files_path + '\\orders.xlsx')
+payments_data = pd.read_excel(source_files_path + '\\order_payment.xlsx')
+
+# ======================================================================================================================
+# Looking around/Analysing the data
+# ======================================================================================================================
+
+orders_data.info() # three columns related to order_approval and order_deliveries (2) has null values
+customers_data.info() # shows no null values
+payments_data.info() # shows few (actually 2) nulls in payments value
